d89b81000c279df0435814e61c496706eadd15e1
rohit-negi-dxc <rohit.negi-dxc@hpe.com>
2020-06-10 17:35:46 +0530

    First Commit

---
+++ WebAPI/WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        EmployeeEntities db = new EmployeeEntities();
+
+        public HttpResponseMessage Index()
+        {
+            var employees = db.tblEmployees.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, employees);
+        }
+    }
+}
